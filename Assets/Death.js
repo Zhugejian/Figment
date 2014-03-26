f882201ea5a641da72425bb83b4b8fb923d862ff
Zhugejian <eric.zhu1101@sbcglobal.net>
2014-03-26 18:37:40 -0400

    health mechanic started

--- Assets/Death.js
+++ Assets/Death.js
@@ -1,4 +1,23 @@
-﻿function OnMouseDown()
+﻿var Health : int = 3;
+
+var x : int = 0;
+
+var mHurt : Material;
+var normal : Material;
+
+function BackToNormal()
 {
-    renderer.transform.position = Vector3(5,-100,65);
+    renderer.material = normal;
+
+}
+
+function OnMouseDown()
+{
+    Health--;
+    renderer.material = mHurt;
+    setTimeout(BackToNormal(), 3000);
+    if (Health == 0)
+    {
+        renderer.transform.position = Vector3(5,-100,65);
+    }
 }
