a35dfba435885406dd8348d5df3f90610bd7ef05
Zhugejian <eric.zhu1101@sbcglobal.net>
2014-04-02 16:25:19 -0400

    small fixes to Room2 and Destroy.cs

--- Assets/Destroy.cs
+++ Assets/Destroy.cs
@@ -3,21 +3,21 @@ using System.Collections;
 
 public class Destroy : MonoBehaviour {
 
-	int Health = 3;
+    int Health = 3;
 
     void Update () {
 
-    	if (Input.GetMouseButtonDown(0)) {
-		    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		    RaycastHit hit;
-		    if(Physics.Raycast(ray, out hit)) {
-				if(hit.collider.gameObject.name == "Enemy") {
-					Health--;
-					if (Health == 0) {
-						Destroy(this.gameObject);
-					}
-				}
-    		}
-	    }
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit)) {
+                if(hit.collider.gameObject.name == "Enemy") {
+                    Health--;
+                    if (Health == 0) {
+                        Destroy(this.gameObject);
+                    }
+                }
+            }
+        }
     }
 }
