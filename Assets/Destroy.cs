1ee248a27e9f61076264dd8745f8073c8b2c10c9
Zhugejian <eric.zhu1101@sbcglobal.net>
2014-04-02 16:12:54 -0400

    Destroy.cs script implemented in Room2

---
+++ Assets/Destroy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+	int Health = 3;
+
+    void Update () {
+
+    	if (Input.GetMouseButtonDown(0)) {
+		    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		    RaycastHit hit;
+		    if(Physics.Raycast(ray, out hit)) {
+				if(hit.collider.gameObject.name == "Enemy") {
+					Health--;
+					if (Health == 0) {
+						Destroy(this.gameObject);
+					}
+				}
+    		}
+	    }
+    }
+}
